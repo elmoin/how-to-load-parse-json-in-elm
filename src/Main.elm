efd03ab430fd77c40ee4b2724fe469cc1a7e0e70
sectore <info@websector.de>
2016-09-06 16:52:05 +0200

    empty project

--- src/Main.elm
+++ src/Main.elm
@@ -4,36 +4,13 @@ import Html exposing (..)
 import Html.App as Html
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Http
-import Json.Decode as Decode exposing ((:=))
-import Task
 
 
 -- MODEL
 
 
 type alias Model =
-    { users : Users
-    , statusMsg : String
-    }
-
-
-type alias Users =
-    List User
-
-
-type alias User =
-    { id : Int
-    , name : String
-    , username : String
-    , address : Address
-    }
-
-
-type alias Address =
-    { street : String
-    , city : String
-    , zipcode : String
+    { statusMsg : String
     }
 
 
@@ -43,59 +20,17 @@ type alias Address =
 
 type Msg
     = LoadUsers
-    | UsersSuccess Users
-    | UsersError Http.Error
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        LoadUsers ->
-            ( { model | statusMsg = "...loading ..." }, getUsers )
-
-        UsersSuccess users ->
-            ( { model | users = users }, Cmd.none )
-
-        UsersError error ->
-            ( { model | statusMsg = "error trying to load users" }, Cmd.none )
+        _ ->
+            ( model, Cmd.none )
 
 
 
 -- VIEW
-
-
-usersView : Users -> Html Msg
-usersView users =
-    ul [ class "mdl-list" ]
-        (List.map userView users)
-
-
-userView : User -> Html Msg
-userView user =
-    let
-        address =
-            user.address
-    in
-        li [ class "mdl-list__item mdl-list__item--three-line" ]
-            [ span [ class "mdl-list__item-primary-content" ]
-                [ i [ class "material-icons mdl-list__item-avatar" ] [ text "person" ]
-                , span [] [ text user.name ]
-                , span [ class "mdl-list__item-text-body" ]
-                    [ text <|
-                        address.zipcode
-                            ++ " "
-                            ++ address.city
-                            ++ ", "
-                            ++ address.street
-                    ]
-                ]
-            , span [ class "mdl-list__item-secondary-content" ]
-                [ span [ class "mdl-list__item-secondary-info" ]
-                    [ text user.username
-                    ]
-                , i [ class "material-icons" ] [ text "star" ]
-                ]
-            ]
 
 
 view : Model -> Html Msg
@@ -106,44 +41,8 @@ view model =
             [ a
                 [ class "mdl-button mdl-js-button mdl-button--raised btn-more", onClick LoadUsers ]
                 [ text "Load Users" ]
-            , if List.isEmpty model.users then
-                div [ class "mdl-typography--headline" ] [ text model.statusMsg ]
-              else
-                usersView model.users
             ]
         ]
-
-
-
--- HTTP
-
-
-getUsers : Cmd Msg
-getUsers =
-    Http.get usersDecoder "./users.json"
-        |> Task.perform UsersError UsersSuccess
-
-
-usersDecoder : Decode.Decoder Users
-usersDecoder =
-    Decode.list userDecoder
-
-
-userDecoder : Decode.Decoder User
-userDecoder =
-    Decode.object4 User
-        ("id" := Decode.int)
-        ("name" := Decode.string)
-        ("username" := Decode.string)
-        ("address" := addressDecoder)
-
-
-addressDecoder : Decode.Decoder Address
-addressDecoder =
-    Decode.object3 Address
-        ("city" := Decode.string)
-        ("street" := Decode.string)
-        ("zipcode" := Decode.string)
 
 
 
@@ -152,7 +51,7 @@ addressDecoder =
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model [] "", Cmd.none )
+    ( Model "", Cmd.none )
 
 
 main : Program Never
